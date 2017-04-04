11e54023e415c000955ad49da7f7509397fcae4f
Christian Kim <cykim@cpp.edu>
2017-04-03 17:12:21 -0700

    Implemented multiple simultaneous clients.

--- EchoClient.java
+++ EchoClient.java
@@ -10,16 +10,17 @@ public final class EchoClient {
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             OutputStream os = socket.getOutputStream();
-            PrintStream out = new PrintStream(os,true,"UTF-8");
+            PrintStream out = new PrintStream(os, true, "UTF-8");
             Scanner kb = new Scanner(System.in);
-    		String client;
-	        while(true){
-                System.out.print("Client> "); 
+            String client;
+            while (true) {
+                System.out.print("Client> ");
                 out.printf(client = kb.nextLine() + "%n");
                 client = br.readLine();
-                if(client.equals("exit")) break;
+                if (client.equals("exit"))
+                    break;
                 System.out.println("Server> " + client);
-	        }
-        } 
+            }
+        }
     }
 }
