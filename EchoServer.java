05df9b4ca5f3c5c63314ac0f12f258892ef4823e
Christian Kim <cykim@cpp.edu>
2017-04-03 17:05:20 -0700

    Implemented multiple simultaneous clients.

--- EchoServer.java
+++ EchoServer.java
@@ -1,27 +1,42 @@
-
 import java.io.*;
 import java.net.*;
+
 public final class EchoServer {
+    private static class EchoThread extends Thread {
+        private Socket socket = null;
+        private String address;
+        public EchoThread(Socket s) {
+            this.socket = s;
+            address = s.getInetAddress().getHostAddress();
+        }
+        public void run() {
+            try {
+
+                System.out.printf("Client connected: %s%n", address);
+                OutputStream os = socket.getOutputStream();
+                PrintStream out = new PrintStream(os, true, "UTF-8");
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                String client;
+                while ((client = br.readLine()) != null) {
+                    out.println(client);
+                }
+                System.out.printf("Client disconnected: %s%n", address);
+            } catch (IOException e) {
+                System.out.printf("Client Failed.%n");
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
+        ServerSocket serverSocket = new ServerSocket(22222);
+        try {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-                    BufferedReader br = new BufferedReader(isr);
-    
-                    String client;
-                    while((client = br.readLine()) != null) {
-                        out.println(client);
-                    }
-                    System.out.printf("Client disconnected: %s%n", address);  
-                }
+                new EchoThread(serverSocket.accept()).start();
             }
+        } finally {
+            serverSocket.close();
         }
     }
 }
